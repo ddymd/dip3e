d1c87f4098f22eb15bbf8f6ddf3fe2ee8edb6f04
junjun.dong <dongjunjun1013@126.com>
2023-10-10 17:32:45 +0800

    update

--- DIP3E_Original/NG04-Laplacian_FrequencyDomain.py
+++ DIP3E_Original/NG04-Laplacian_FrequencyDomain.py
@@ -10,56 +10,65 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def fft_im(im, plot: plt.Axes):
+def fft_im(im, plot: plt.Axes = None):
     gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     rows, cols = gray_im.shape[:2]
-    M, N = rows*2, cols*2
+    M, N = cv2.getOptimalDFTSize(rows), cv2.getOptimalDFTSize(cols)
+    print('origin size({},{}) vs padded size({},{})'.format(rows, cols, M, N))
+
+    # normal
+    normal_im = np.zeros_like(gray_im)
+    cv2.normalize(gray_im, normal_im, norm_type=cv2.NORM_MINMAX)
+
+    # pad image
     ext_im = np.zeros((M, N), dtype=np.float32)
-    ext_im[0:rows, 0:cols] = gray_im
+    ext_im[0:rows, 0:cols] = normal_im
     # fft and shift
     fft = np.fft.fft2(ext_im)
     fft_shift = np.fft.fftshift(fft)
-
-    # spectrum = np.log(np.abs(fft_shift))
-    plot.set_title('origin')
-    plot.imshow(gray_im, cmap='gray')
-
-    return fft_shift, ext_im[0:rows, 0:cols]
+    if plot is not None:
+        spectrum = np.log(np.abs(fft_shift))
+        plot.set_title('spectrum')
+        plot.imshow(gray_im, cmap='gray')
+        plot.set_xticks([])
+        plot.set_yticks([])
+    return fft_shift
 
 def apply_laps(fft):
     M, N = np.shape(fft)
-    rows, cols = int(M/2), int(N/2)
-    mask = np.zeros((M,N))
+    cm, cn = int(M/2), int(N/2)
+    H = np.zeros((M,N))
     for u in range(M):
         for v in range(N):
-            mask[u,v] = -4*np.pi**2*((u-rows)**2+(v-cols)**2)
-    filtered_im = fft * mask
+            H[u,v] = -4*(np.pi**2)*((u-cm)**2+(v-cn)**2)
+    filtered_im = fft * H
     ifft_shift = np.fft.ifftshift(filtered_im)
     ifft = np.fft.ifft2(ifft_shift)
-    crop = ifft[0:rows, 0:cols]
-    # delta = np.abs(crop)
-    return crop
+    return np.abs(ifft)
 
 if __name__ == '__main__':
     figure = plt.figure()
 
     im = cv2.imread('CH04_Images/Fig0458(a)(blurry_moon).tif')
+    rows, cols = im.shape[:2]
     gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-    plot = figure.add_subplot(121)
-    plot.set_xticks([])
-    plot.set_yticks([])
-    fft, uim = fft_im(im, plot)
+    fft = fft_im(im, figure.add_subplot(121))
 
-    delta_f = apply_laps(fft)
+    delta_f = np.exp(np.log(apply_laps(fft)))
+    # crop
+    delta_f = delta_f[0:rows, 0:cols]
+    print(delta_f, np.max(delta_f), np.min(delta_f))
 
-    delta = delta_f.real
-    delta = delta / (np.max(delta) - np.min(delta))
-    res = gray_im - delta
-    # res = (res - np.min(res)) / (np.max(res) - np.min(res)) * 255
+    # normal
+    # normal_im = np.zeros_like(gray_im)
+    # cv2.normalize(gray_im, normal_im, norm_type=cv2.NORM_MINMAX)
+
+    res = gray_im - delta_f
     print(res)
     plotr = figure.add_subplot(122)
     plotr.imshow(res, cmap='gray')
+    plotr.set_title('result')
     plotr.set_xticks([])
     plotr.set_yticks([])
 
--- DIP3E_Original/NG05-Bandpass(Bandreject)Filtering.py
+++ DIP3E_Original/NG05-Bandpass(Bandreject)Filtering.py
@@ -8,25 +8,22 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def fft_im(im, plot: plt.Axes):
+def fft_im(im, plot: plt.Axes = None):
     gray_im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     rows, cols = gray_im.shape[:2]
-    M, N = rows*2, cols*2
+    M, N = cv2.getOptimalDFTSize(rows), cv2.getOptimalDFTSize(cols)
+    print('origin size({},{}) vs padded size({},{})'.format(rows, cols, M, N))
     ext_im = np.zeros((M, N))
     ext_im[0:rows, 0:cols] = gray_im
     # fft and shift
     fft = np.fft.fft2(ext_im)
     fft_shift = np.fft.fftshift(fft)
-
-    spectrum = np.log(np.abs(fft_shift))
-    # test = np.ones((M,N))
-    # test[spectrum>10] = 0
-    # test[rows-130:rows+130,cols-130:cols+130] = 1
-    plot.set_title('spectrum')
-    plot.imshow(spectrum, cmap='gray')
-    plot.set_xticks([])
-    plot.set_yticks([])
-
+    if plot is not None:
+        spectrum = np.log(np.abs(fft_shift))
+        plot.set_title('spectrum')
+        plot.imshow(spectrum, cmap='gray')
+        plot.set_xticks([])
+        plot.set_yticks([])
     return fft_shift
 
 def apply_bbp(fft, n, d0, w, plot: plt.Axes):
@@ -37,19 +34,19 @@ def apply_bbp(fft, n, d0, w, plot: plt.Axes):
     M, N = np.shape(fft)
     uu,vv = int(M/2), int(N/2)
     H = np.ones((M,N))
-    H1 = np.ones((M,N))
-    H2 = np.ones((M,N))
     for u in range(M):
         for v in range(N):
             powD = (u-uu)**2 + (v-vv)**2
-            H[u,v] = 1/(1+(np.sqrt(powD)*w/(powD-d0**2))**(2*n))
-            H1[u,v] = 1/(1+(np.sqrt(powD)*w/(powD-(d0-90)**2))**(2*n))
-            H2[u,v] = 1/(1+(np.sqrt(powD)*w/(powD-(d0+100)**2))**(2*n))
-    filtered_fft = H * fft * H1 * H2
+            if powD - d0**2 == 0:
+                H[u,v] = 0
+            else:
+                H[u,v] = 1/(1+(np.sqrt(powD)*w/(powD-d0**2))**(2*n))
+    filtered_fft = H * fft
 
     # H_show = np.uint8(255 * (H - np.min(H)) / (np.max(H) - np.min(H)))
     # print(H_show)
     plot.set_title('applied-spectrum')
+    filtered_fft[filtered_fft==0] = 1
     plot.imshow(np.log(np.abs(filtered_fft)), cmap='gray')
     # plot.imshow(H_show, cmap='gray')
     plot.set_xticks([])
@@ -57,12 +54,13 @@ def apply_bbp(fft, n, d0, w, plot: plt.Axes):
 
     ifft_shift = np.fft.ifftshift(filtered_fft)
     ifft = np.fft.ifft2(ifft_shift)
-    crop = ifft[0:uu,0:vv]
-    return np.abs(crop)
+    # crop = ifft[0:uu,0:vv]
+    return np.abs(ifft)
 
 if __name__ == '__main__':
     # 获取不到一个准确的频谱
     im = cv2.imread('CH05_Images/Fig0516(a)(applo17_boulder_noisy).tif')
+    rows, cols = im.shape[:2]
     figure = plt.figure()
     plot = figure.add_subplot(221)
     plot.set_title('origin')
@@ -71,7 +69,8 @@ if __name__ == '__main__':
     plot.set_yticks([])
 
     fft = fft_im(im, figure.add_subplot(222))
-    im_back = apply_bbp(fft, 3, 350, 100, figure.add_subplot(223))
+    im_back = apply_bbp(fft, 4, 190, 30, figure.add_subplot(223))
+    im_back = im_back[0:rows, 0:cols]
 
     plotr = figure.add_subplot(224)
     plotr.set_title('results')

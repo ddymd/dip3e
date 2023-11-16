401dd95d3e45032a10b72c7d1f606fdaf38e9cf3
jack <dongjunjun1013@126.com>
2023-11-17 00:53:20 +0800

    update prog

--- init.el
+++ init.el
@@ -57,13 +57,13 @@
 (defun next-ten-lines()
   "Move cursor to next 10 lines."
   (interactive)
-  (next-line 10))
+  (forward-line 10))
 (global-set-key (kbd "M-n") 'next-ten-lines)
 
 (defun previous-ten-lines()
   "Move cursor to previous 10 lines."
   (interactive)
-  (previous-line 10))
+  (forward-line -10))
 (global-set-key (kbd "M-p") 'previous-ten-lines)
 
 (global-set-key (kbd "C-j") nil)
@@ -92,7 +92,7 @@
   :ensure t)
 
 (use-package ivy
-  :ensure t                          ; 确认安装，如果没有安装过 ivy 就自动安装    
+  :ensure t                          ; 确认安装，如果没有安装过 ivy 就自动安装
   :init                              ; 在加载插件前执行命令
   (ivy-mode 1)                       ; 启动 ivy-mode
   (counsel-mode 1)
@@ -301,12 +301,57 @@ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cu
   :hook (prog-mode . rainbow-delimiters-mode))
 
 ;; 插件 - 编程开发类 *********************************************************
+;; 代码补全功能
+(use-package company
+  :ensure t
+  :init (global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1)
+  (setq company-tooltip-align-annotations t)
+  (setq company-idle-delay 0.0)
+  ;; (setq company-show-numbers t)
+  (setq company-show-quick-access t)
+  (setq company-selection-wrap-around t)
+  (setq company-transformers '(company-sort-by-occurrence)))
+;; UI 界面显示一个box
+;; (use-package company-box
+;;   :ensure t
+;;   :if window-system
+;;   :hook (company-mode . company-box-mode))
 
 ;; 编程模式下代码语法检查
 (use-package flycheck
- :ensure t
+  :ensure t
+  :config (setq truncate-lines nil) ; 如果单行信息很长会自动换行
  :hook
  (prog-mode . flycheck-mode))
+
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l" lsp-file-watch-threshold 500)
+  :hook (lsp-mode . lsp-enable-which-key-integration)
+  :config
+  ;;(setq lsp-completion-provider :none)
+  (setq lsp-headerline-breadcrumb-enable t)
+  :bind ("C-c l s" . lsp-ivy-workspace-symbol)) ;; 可快速搜索工作区内的符号（类名、函数名、变量名等）
+
+;; lsp ui
+(use-package lsp-ui
+  :ensure t
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  (setq lsp-ui-doc-position 'top))
+
+(use-package lsp-ivy :ensure t :after (lsp-mode))
+
+(use-package c++-mode
+  :functions c-toggle-hungry-state
+  :hook
+  (c-mode . lsp-deferred)
+  (c++-mode . lsp-deferred)
+  (c++-mode . c-toggle-hungry-state))
 
 ;; Themes
 (load-theme 'dracula t)
@@ -326,8 +371,9 @@ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cu
    '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
  '(display-line-numbers-type 'relative)
  '(global-display-line-numbers-mode t)
+ '(ispell-dictionary nil)
  '(package-selected-packages
-   '(all-the-icons tiny cmake-mode which-key use-package-hydra undo-tree swiper-helm smart-mode-line rainbow-delimiters mwim multiple-cursors marginalia ivy-avy hydra highlight-symbol good-scroll flycheck embark dashboard counsel amx ace-window))
+   '(lsp-ivy lsp-ui lsp-mode company-tabnine company-box projectile company all-the-icons tiny cmake-mode which-key use-package-hydra undo-tree swiper-helm smart-mode-line rainbow-delimiters mwim multiple-cursors marginalia ivy-avy hydra highlight-symbol good-scroll flycheck embark dashboard counsel amx ace-window))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

cdf721593ae0b0732f5026e2b2ca53be59e1a7fa
junjun.dong <dongjunjun1013@126.com>
2023-11-29 14:42:00 +0800

    update morphology on opening by reconstruction

--- DIP3E_Original/CH09-OpeningReconstruction.py
+++ DIP3E_Original/CH09-OpeningReconstruction.py
@@ -1,23 +1,42 @@
 #!/bin/env python
 #
-# 形态学重建 - 开运算
+# 形态学重建 - 重建开运算(Opening by Reconstruction)
 #
 
 import cv2
 import numpy as np
 import matplotlib.pylab as plt
 
+def obrc(bin_im, zero_arg, i: int = 0):
+    '''重建开运算 Opening By Reconstruction'''
+    dilate_kernel = np.ones((3,3))
+    dilate_im = cv2.dilate(bin_im, dilate_kernel)
+    dilate_im[zero_arg] = 0
+    i = i + 1
+    if (bin_im == dilate_im).all():
+        return i, dilate_im
+    return obrc(dilate_im, zero_arg, i)
+
 def reconstruction(bin_im, plot: plt.Axes = None):
+    # 获取锚点
     mark_kernel = np.ones((51,1))
     mark_im = cv2.erode(bin_im, mark_kernel)
+    # 重建开运算
+    loop_count, recons_im = obrc(mark_im, bin_im == 0)
 
-    dilate_kernel = np.ones((3,3))
+    if plot is not None:
+        plot.set_title('reconstruction: {}'.format(loop_count))
+        plot.set_xticks([])
+        plot.set_yticks([])
+        plot.imshow(recons_im, cmap='gray')
+    return recons_im
 
 def reconstruction_open(bin_im, plot: plt.Axes = None):
+    '''开运算: 先腐蚀再膨胀'''
     kernel = np.ones((51,1))
     open_im = cv2.morphologyEx(bin_im, cv2.MORPH_OPEN, kernel)
     if plot is not None:
-        plot.set_title('open image')
+        plot.set_title('mark by open(erode+dilate)')
         plot.set_xticks([])
         plot.set_yticks([])
         plot.imshow(open_im, cmap='gray')
@@ -28,7 +47,7 @@ def reconstruction_mark(bin_im, plot: plt.Axes = None):
     kernel = np.ones((51,1))
     erode_im = cv2.erode(bin_im, kernel)
     if plot is not None:
-        plot.set_title('mark image')
+        plot.set_title('mark by erode')
         plot.set_xticks([])
         plot.set_yticks([])
         plot.imshow(erode_im, cmap='gray')
@@ -49,5 +68,7 @@ if __name__ == '__main__':
 
     mark_im = reconstruction_mark(bin_im, figure.add_subplot(222))
     open_im = reconstruction_open(bin_im, figure.add_subplot(223))
+
+    reconstruction(bin_im, figure.add_subplot(224))
 
     plt.show()
